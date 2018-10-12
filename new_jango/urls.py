e67f13d28f25e6d7c5a8f9ba6aaa66b62ab529bc
Mahmoud Abo Hussien <mahmoud_abo_hussien@codersea.com>
2018-10-12 18:15:45 +0200

    make user profile and picture

--- new_jango/urls.py
+++ new_jango/urls.py
@@ -17,6 +17,10 @@ from django.contrib import admin
 # we added include to add new apps Urls
 from django.urls import path, include
 from users import views as user_view
+from django.contrib.auth import views as auth_views
+# these both import to view user profile images
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,8 +33,15 @@ urlpatterns = [
     # path('blog/', include('blog.urls')),
     path('', include('blog.urls')),
 
-
     # we used the view directly of users
-    path('register/',user_view.register, name="register"),
+    path('register/', user_view.register, name="register"),
+    # this way of using views called class based Views "auth_views.LoginView.as_view"
+    # we can pass template name to the view to make our design to the html
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name="login"),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name="logout"),
+    path('profile/', user_view.profile, name="profile"),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/views.py
+++ users/views.py
@@ -4,6 +4,8 @@ from django.contrib.auth.forms import UserCreationForm
 # import flash messages
 from django.contrib import messages
 from .forms import UserRegisterForm
+# login required decorator to use it on ur function to prevent unlogined users
+from django.contrib.auth.decorators import login_required
 
 
 # messages.debug()
@@ -22,9 +24,13 @@ def register(request):
             # get request data from cleaned_data dict
             username = form.cleaned_data.get('username')
             # add new msg to flash
-            messages.success(request, 'Account Created For {}!'.format(username))
+            messages.success(request, 'Your Account Has Been Created! You Are Now Able To Login.')
             # redirect to blog home page
-            return redirect('blog-home')
+            return redirect('login')
     else:
         form = UserRegisterForm()
-    return render(request, 'users/register.html', {'form': form})
+    return render(request, 'users/register.html', {'form': form, 'title': 'Register'})
+# decoration adds a functionality( code like : if or some checks ) to our function
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html', {'title': 'Profile'})
