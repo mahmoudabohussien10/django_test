cd1d46fcf0ff6e6068d9157c56430138a51bd443
Mahmoud Abo Hussien <mahmoud_abo_hussien@codersea.com>
2018-10-15 20:44:38 +0200

    add login , auth , user profile edit form

--- users/views.py
+++ users/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 # import flash messages
 from django.contrib import messages
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 # login required decorator to use it on ur function to prevent unlogined users
 from django.contrib.auth.decorators import login_required
 
@@ -30,7 +30,31 @@ def register(request):
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form, 'title': 'Register'})
+
+
 # decoration adds a functionality( code like : if or some checks ) to our function
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html', {'title': 'Profile'})
+    if request.method == "POST":
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(
+            request.POST,
+            request.FILES,
+            instance=request.user.profile
+            )
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, 'Your Account Has Been Updated!')
+            # redirect to blog home page
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'title': 'Profile',
+        'u_form': u_form,
+        'p_form': p_form
+    }
+    return render(request, 'users/profile.html', context)
